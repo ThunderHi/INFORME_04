8cc1f9a35caf6969b783eb31ee27deefb8cd7012
AngelSalasZapata <164601086+AngelSalasZapata@users.noreply.github.com>
2024-04-13 18:06:31 -0500

    Update ACT1.py

--- CODIGOS_PYTHON/ACT1.py
+++ CODIGOS_PYTHON/ACT1.py
@@ -1,19 +1,18 @@
-#Clase Padre
-class Padre:
+class Padre:    #Clase Padre
     def __init__(self, nombre):
         self.nombre = nombre  #Atributo "nombre"
         self.hijos = []       #Lista para almacenar objetos Hijo
         self.numHijos = 0     #Contador de hijos
-    
+
     #Metodo para agregar un hijo a la lista
     def agregarHijo(self, hijo):
-        if self.numHijos < 5:
+        if self.numHijos < 7:
             self.hijos.append(hijo)
             self.numHijos += 1
         else:
             print("No se pueden agregar más hijos.")
-
-    #Metodo para imprimir información del Padre
+    
+    #Metodo para imprimir informacion del Padre
     def imprimirInfo(self):
         print("Nombre del Padre:", self.nombre)
         print("Número de Hijos:", self.numHijos)
@@ -25,13 +24,16 @@ class Hijo:
 
 #Función principal
 def main():
-    padre = Padre("Alexander")  #Crea un objeto Padre con nombre "Alexander"
-
-    hijo1 = Hijo("Pedro")  #Crea un objeto Hijo con nombre "Pedro"
-    hijo2 = Hijo("Maria")  #Crea un objeto Hijo con nombre "Maria"
+    padre = Padre("Alejandro")  #Crea un objeto Padre
+    hijo1 = Hijo("Diego")  #Crea un objeto Hijo
+    hijo2 = Hijo("Carmen")
+    hijo3 = Hijo("Joaquin")
+    hijo4 = Hijo("Mariana")
 
     padre.agregarHijo(hijo1)  #Agrega hijo1 al Padre
     padre.agregarHijo(hijo2)  #Agrega hijo2 al Padre
+    padre.agregarHijo(hijo3)  #Agrega hijo2 al Padre
+    padre.agregarHijo(hijo4)  #Agrega hijo2 al Padre
 
     padre.imprimirInfo()  #Imprime la información del Padre
 
